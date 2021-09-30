7ace4ae2fa378063db3c97dc924dbf1c00e096cd
nepit19 <91637044+nepit19@users.noreply.github.com>
2021-10-01 02:13:17 +0530

    Add files via upload

---
+++ Twitter Scapper using Twitter API/config.py
@@ -0,0 +1,27 @@
+#Created By Navneet Das 
+
+#Paste your API Key
+APIKEY = ''
+
+#Paste your API Secret Key 
+APISECRETKEY = ''
+
+#Paste your Bearer Token 
+BEARERTOKEN = '' 
+
+#Paste your Access Token
+ACCESSTOKEN = ''
+
+#Paste your Access Token Secret  
+ACCESSTOKENSECRET = '' 
+
+#Output File Name
+CSV_FILE_NAME = 'tweets_navneet.csv'
+#I have created 5 basic columns 
+COLS = ["ID","Text","Created At","Username", "Number of Followers"]
+
+#Enter Hashtags here (multiple hashtags are to be entered in the form of: #hash1 OR #hash2)
+HASHTAGS = "" 
+COUNT = 500
+
+#the contents of config file can be pasted in main file too but for cleaner code I have made a seperate file
