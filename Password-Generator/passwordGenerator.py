15f47f2979cf8106f3a4a443771fe4e2482f9d04
hamzaazman <hamzaazman@outlook.com>
2021-10-01 19:37:56 +0300

    Add password generator

---
+++ Password-Generator/passwordGenerator.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+def main():
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+
+    plen = int(input('Enter Password Length: '))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    print("Your Password : ",end="")
+    print("" .join(random.sample(s,plen)))
+
+main()
